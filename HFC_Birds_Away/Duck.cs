559b3e45d2065491736dcdf7ccc45f2d2aac7f98
Alex Blackman <alexander.blackman@gmail.com>
2021-01-31 11:48:35 +1100

    Add project files.

---
+++ HFC_Birds_Away/Duck.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace HFC_Birds_Away
+{
+    class Bird
+    {
+        public string Name { get; set; }
+
+        public void Fly(string destination)
+        {
+            Console.WriteLine($"Flap flap flap, {this} is flying to {destination}");
+        }
+
+        public override string ToString()
+        {
+            return $"A bird named {Name}";
+        }
+
+        public static void FlyAway(List<Bird> flock, string destination)
+        {
+            foreach (Bird bird in flock)
+            {
+                bird.Fly(destination);
+            }
+        }
+
+    }
+
+
+
+    class Duck : Bird
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+    
+        public enum KindOfDuck
+        {
+            Mallard,
+            Muscovy,
+            Loon,
+            Peking
+        }
+        public override string ToString()
+        {
+            return $"A {Size}\" {Kind}";
+        }
+
+
+    }
+
+
+    enum SortCriteria
+    {
+        SizeThenKind,
+        KindThenSize,
+    }
+
+    class DuckComparer: IComparer<Duck>
+    {
+        public SortCriteria SortBy = SortCriteria.SizeThenKind;
+
+    public int Compare(Duck x, Duck y)
+        {
+            if (SortBy == SortCriteria.SizeThenKind)
+            {
+                if (x.Size > y.Size)
+                    return 1;
+                else if (x.Size < y.Size)
+                    return -1;
+                else
+                    return 0;
+            }
+            else if (SortBy == SortCriteria.KindThenSize)
+            {
+                if (x.Kind > y.Kind)
+                    return 1;
+                else if (x.Kind < y.Kind)
+                    return -1;
+                else
+                    return 0;
+            }
+            //This superfulous else statement allows us to add new things to compare in the future.
+            else 
+                return 0;
+        }
+    }
+}
---
+++ HFC_Birds_Away/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HFC_Birds_Away
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> myFlock = new List<Duck>()
+            {
+                new Duck(){Size=12, Kind= Duck.KindOfDuck.Loon},
+                new Duck(){Size=15, Kind = Duck.KindOfDuck.Peking},
+                new Duck(){Size=19, Kind = Duck.KindOfDuck.Muscovy},
+                new Duck(){Size = 10, Kind = Duck.KindOfDuck.Mallard},
+                new Duck(){Size = 14, Kind = Duck.KindOfDuck.Muscovy},
+                new Duck(){Size = 15, Kind = Duck.KindOfDuck.Loon}
+            };
+
+           IEnumerable<Bird> upcastFlock = myFlock;
+            Bird.FlyAway(upcastFlock.ToList(), "Minnesota");
+        }
+    }
+}
